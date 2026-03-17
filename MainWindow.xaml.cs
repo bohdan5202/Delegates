02285901acbeaad3072738939808d4757a7eab9e
thinkpadAB <akademiabiaslska.pl>
2026-03-17 15:08:15 +0100

    Simple UI to check point 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,17 +20,39 @@ namespace lab4
     /// </summary>
     public partial class MainWindow : Window
     {
-        delegate double MathOperation();
+        delegate double MathOperation(double a);
         static double Square(double a) => a * a;
         static double SquareRoot(double a) => Math.Sqrt(a);
         static double Reciprocal(double a) => 1 / a;
+        MathOperation operation;
 
 
         public MainWindow()
         {
+            
             InitializeComponent();
         }
 
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (OperationBox.SelectedItem == null)
+                return;
 
+            string selected = OperationBox.SelectedItem.ToString();
+
+            switch (selected)
+            {
+                case "Square":
+                    operation = Square;
+                    break;
+                case "Square Root":
+                    operation = SquareRoot;
+                    break;
+                case "Reciprocal":
+                    operation = Reciprocal;
+                    break;
+            }
+            ResultText.Text = "Selected: " + OperationBox.SelectedItem;
+        }
     }
 }
